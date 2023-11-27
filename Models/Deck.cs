072bb6561b8d3dfbaaa0ab8f5098633dabefc3ee
Asst2Mangr <124299473+Asst2Mangr@users.noreply.github.com>
2023-11-27 10:22:36 -0600

    models

---
+++ Models/Deck.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project2.Models
+{
+    public class Deck
+    {
+        public int DeckID { get; set; }
+        [Required(ErrorMessage = "Please enter a deck name.")]
+        public string? DeckName { get; set;}
+        [Required(ErrorMessage = "Please enter a card count.")]
+        [Range(0, 100, ErrorMessage = "Please enter a number with in range!")]
+        public int DeckCount { get; set; }
+    }
+}
---
+++ Models/DeckContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project2.Models
+{
+    public class DeckContext : DbContext
+    {
+        public DeckContext(DbContextOptions<DeckContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Deck> Decks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Deck>().HasData(
+                new Deck
+                {
+                    DeckID = 1,
+                    DeckName = "Rider-Waite Tarot", 
+                    DeckCount = 78
+                },
+                new Deck
+                {
+                    DeckID = 2,
+                    DeckName = "Standard",
+                    DeckCount = 52
+                },
+                new Deck
+                {
+                     DeckID = 3,
+                     DeckName = "another one",
+                     DeckCount = 100
+                }
+                );
+
+        }
+    }
+}
